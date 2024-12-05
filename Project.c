ca3d6aed507cd6bafc1b8cdf96a0d3a3f5009e37
Advay Anand <advayanand37@gmail.com>
2024-12-05 22:31:18 +0530

    Update Project.c

--- Project.c
+++ Project.c
@@ -138,10 +138,4 @@ void HuffmanCodes(char data[], int freq[], int size) {
     printCodes(root, arr, top);
 }
 
-int main() {
-    char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
-    int freq[] = { 5, 9, 12, 13, 16, 45 };
-    int size = sizeof(arr) / sizeof(arr[0]);
-    HuffmanCodes(arr, freq, size);
-    
-}
+
